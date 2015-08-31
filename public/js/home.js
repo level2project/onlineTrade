46656b7fa04cb876d74317437999784f0951db88
Joe <347126391@qq.com>
2015-08-31 21:04:32 +0800

    商品列表与数据库交互

--- public/js/home.js
+++ public/js/home.js
@@ -53,65 +53,29 @@ $('#log-out').on('click', function () {
 });
 
 /**
- * handlebarjs 渲染商品列表  注意后面还要处理图片问题
+ * ajax获取商品列表 并用handlebarjs 渲染  (注意后面还要处理图片问题)
  */
-var myData = {//先虚构一个data对象用于填充
-    'good': [
-        {summary: 'not long than 13', 'explain': 'not long than 30'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good1.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good2.jpg'},
-        {summary: 'default', 'explain': 'default'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good3.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good4.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good5.jpg'},
-
-        {summary: 'not long than 13', 'explain': 'not long than 30'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good1.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good2.jpg'},
-        {summary: 'default', 'explain': 'default'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good3.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good4.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good5.jpg'},
-        {summary: 'not long than 13', 'explain': 'not long than 30'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good1.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good2.jpg'},
-        {summary: 'default', 'explain': 'default'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good3.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good4.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good5.jpg'},
-        {summary: 'not long than 13', 'explain': 'not long than 30'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good1.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good2.jpg'},
-        {summary: 'default', 'explain': 'default'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good3.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good4.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good5.jpg'},
-        {summary: 'not long than 13', 'explain': 'not long than 30'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good1.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good2.jpg'},
-        {summary: 'default', 'explain': 'default'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good3.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good4.jpg'},
-        {summary: '一', 'explain': '-', 'pictureAddress': 'http://10.33.28.7:15000/good5.jpg'}
-    ]
-}
+var myData = null;
+$.get('/verify/getGoods', function (text, status) {
+    myData = JSON.parse(text);
+    $.each(myData, function (i, obj) {      //对象good 遍历
+        fill("good-template", "fill-good", obj);  // 参数为模板id 最终加入DOM结构ID 需添加的数据
+    });
+    /**
+     * 处理图片
+     */
+    var temp = $('#goods>div>div');
+    for (var i = 0; i < temp.length; i++) {
+        if ($(temp[i]).html()) {
+            //console.log($(temp[i]).html());
+            $(temp[i]).next().children(1).attr('src', $(temp[i]).html());
+        }
+    }
+});
 function fill(template_id, fill_id) {     //  获取指定元素id模板 填充数据data后 插回到到id前
     var source = $("#" + template_id).html();                 //取得模板
     //console.log(source);
     var template = Handlebars.compile(source);
     var result = template(arguments[2]);           //将数据 填充到模板
     $("#" + fill_id).before(result);//整个模块显示的地方
-}
-$.each(myData.good, function (i, obj) {      //对象good 遍历
-    fill("good-template", "fill-good", obj);  // 参数为模板id 最终加入DOM结构ID 需添加的数据
-})
-//处理图片
-var temp = $('#goods>div>div');
-for (var i = 0; i < temp.length; i++) {
-    if ($(temp[i]).html()) {
-        //console.log($(temp[i]).html());
-        console.log($(temp[i]).html().indexOf('public'))
-        $(temp[i]).next().children(1).attr('src', $(temp[i]).html());
-
-    }
 }
