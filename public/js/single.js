2b190cbbb302a6c9928437ee527767c60fb3f6d0
Joe <347126391@qq.com>
2015-09-04 11:45:55 +0800

    商品页幻灯片异步问题解决 小错误修正

--- public/js/single.js
+++ public/js/single.js
@@ -8,6 +8,35 @@ $(document).ready(function () {
     $(".memenu").memenu();
 });
 
+
+// Can also be used with $(document).ready()
+$(window).load(function () {
+    var flag = true;
+
+    $('a[tabindex]').on('click', function () {
+        var alreadyLogin = document.cookie;
+        if (alreadyLogin.indexOf('userName=') == -1) {
+            if (confirm('请先登录')) {
+                window.location.href = 'login.html';
+            } else {
+                $('a[tabindex]').popover('hide');
+            }
+        } else {
+            $('a[tabindex]').popover({
+                animation: true,
+                delay: { "show": 200, "hide": 100 },
+                html: true
+            });
+            if (flag) {
+                $('a[tabindex]').popover('show');
+                flag = false;
+            }
+
+        }
+    });
+});
+
+
 /**
  * 从locantion中获取商品的pid
  */
@@ -18,7 +47,7 @@ a.setAttribute('href', window.location.href);
  * 请求、渲染对应商品信息
  */
 $.get('/verify/goodDetail', {pid: a.search.substr(6)}, function (text, status) {
-    //console.log(text);
+    console.log(text);
     var Data = JSON.parse(text)[0];
     if (Data.pname)
         $('#pname').html(Data.pname);
@@ -41,11 +70,27 @@ $.get('/verify/goodDetail', {pid: a.search.substr(6)}, function (text, status) {
         $('#picture3').attr('src', Data.picture3).parent().attr('data-thumb', Data.picture3);
         $('.flex-control-nav :eq(2)>img').attr('src', $('#picture3').attr('src'));
     }
+    if(typeof ($('#picture2').attr('src'))=='undefined'){
+        $('#picture2').parent().remove();
+    }
+    if(typeof ($('#picture3').attr('src'))=='undefined'){
+        $('#picture3').parent().remove();
+    }
+    //图片加载完毕 开始幻灯片渲染播放
+    $('.flexslider').flexslider({
+        minItems:1,
+        animation: "fade",
+        controlNav: "thumbnails",
+        pauseOnHover: true,
+        keyboard: false,
+        slideshowSpeed: 25000,
+        mousewheel: true
+    });
 
 
     //下面添加商品的额外信息  后面要考虑重构。。。（未实现。。）
-    if (Data.soldamount)
-        $('#rest').html(Data.amount - Data.soldamount);
+    $('#rest').html('(还剩 ' + (Data.amount - Data.soldamount) + ' 件)');
+
     if (Data.pcstyle)
         $('#pcstyle').html(Data.pcstyle);
     if (Data.pcname)
